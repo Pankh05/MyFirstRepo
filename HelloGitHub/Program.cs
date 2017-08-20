892ca0306b0659dacf07271db3e2ec6d6af61d9b
pankaj <pankaj@hotmail.co.in>
2017-08-21 00:12:06 +0530

    Added  readykey function.

--- HelloGitHub/Program.cs
+++ HelloGitHub/Program.cs
@@ -11,6 +11,7 @@ namespace HelloGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World, This is my India and I love.");
+            Console.ReadKey();
 
         }
     }
